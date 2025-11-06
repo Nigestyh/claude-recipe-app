09833ccc12f8688e2c722a5b9f6f235806f86d8a
Ninz11 <148561307+Nigestyh@users.noreply.github.com>
2025-11-06 20:10:58 +0000

    Create Header component inside components folder with Chef Claude asset

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import chefClaudeLogo from "../assets/chef-claude.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={chefClaudeLogo} />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
+
+export default Header;
